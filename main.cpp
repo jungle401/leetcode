63ed5e05b845bc3652c0e47e09f6ac653e551c2f
johnson <johnson056029@gmail.com>
2022-08-21 09:20:49 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        int curPos = startFuel;
+        int ans = 0;
+        priority_queue<int> pq;
+        int it_stat = 0;
+        while (true) {
+						// enlist all available fuel, of whose corresponding stations being prior (or equal) to the current position.
+            while (it_stat < stations.size() && stations[it_stat][0] <= curPos) {
+                pq.push(stations[it_stat][1]);
+                it_stat++;
+            }
+						// move one maximum fuel in list forward
+            if (!pq.empty() && curPos < target) {
+                curPos += pq.top();
+                pq.pop();
+                ans++;
+            } else if (curPos >= target) {
+                return ans;
+            } else if (pq.empty()) {
+                if (curPos < target) {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+};
