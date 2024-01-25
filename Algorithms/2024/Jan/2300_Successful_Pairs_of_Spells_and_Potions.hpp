8008241b50971644e4987376b6f897238b831f30
jungle401 <johnson401.cs09@nycu.edu.tw>
2024-01-25 20:05:42 +0800

    updated

---
+++ Algorithms/2024/Jan/2300_Successful_Pairs_of_Spells_and_Potions.hpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        auto res = vector<int>();
+        for (auto& i : spells) {
+            auto tmp = 0;
+            auto lb = lower_bound(potions.begin(), potions.end(), i, [&success] (const long long& element, const auto& val) {
+                return element * val < success;
+            });
+            res.push_back(distance(lb, potions.end()));
+        }
+        return res;
+    }
+};
